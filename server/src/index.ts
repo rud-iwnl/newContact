d26af81e7d2ad2770da5f6dcf8d93cc1bf21d01b
Leonty Filichkin <headleo99@gmail.com>
2025-07-01 18:21:45 +0300

    fix: set CORS origin to '*' for Socket.IO, only relative paths in Express

--- server/src/index.ts
+++ server/src/index.ts
@@ -13,8 +13,8 @@ app.use(express.static(path.join(__dirname, '../public')));
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
-    methods: ["GET", "POST"]
+    origin: '*',
+    methods: ['GET', 'POST']
   }
 });
 
